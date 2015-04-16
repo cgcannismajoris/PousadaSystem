d51ae4ca9b14cf0a18deaeeb3e390c0a6aa6ca57
cristianmello <cristianc.mello@gmail.com>
2015-04-16 16:17:03 -0300

    Criação do projeto básico no NetBeans 7

--- src/pousadasystem/PousadaSystem.java
+++ src/pousadasystem/PousadaSystem.java
@@ -1,10 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package pousadasystem;
 
-public class PousadaSystem 
-{
-    
-    public static void main(String[] args) 
-    {
-        System.out.println("Projeto Pousada System.");
+/**
+ *
+ * @author alunolab09
+ */
+public class PousadaSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
     }
 }
