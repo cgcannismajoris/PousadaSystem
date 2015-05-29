5165f8003d3413e89689884d8b4326bde49692e9
cristianmello <cristianc.mello@gmail.com>
2015-05-29 20:53:55 -0300

    - Manutenção e Adição de classes de modelo, fábrica e controle (unidade de testes); - Inclusão do Bootstrap.

---
+++ src/java/modelo/Cliente.java
@@ -0,0 +1,9 @@
+package modelo;
+
+public class Cliente extends Pessoa
+{
+    public Cliente() 
+    {
+        this.privilegio = 2;
+    }
+}
---
+++ src/java/modelo/Proprietario.java
@@ -0,0 +1,9 @@
+package modelo;
+
+public class Proprietario extends Pessoa
+{
+    public Proprietario()
+    {
+        this.privilegio = 0;
+    }
+}
