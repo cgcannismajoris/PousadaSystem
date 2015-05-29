67732edfe44bfdde16a1caea03033da513b95acd
cristianmello <cristianc.mello@gmail.com>
2015-05-29 20:53:55 -0300

    - Manutenção e Adição de classes de modelo, fábrica e controle (unidade de testes); - Inclusão do Bootstrap.

---
+++ src/java/factories/PessoaFactory.java
@@ -0,0 +1,24 @@
+package factories;
+
+import modelo.Administrador;
+import modelo.Cliente;
+import modelo.Pessoa;
+import modelo.Proprietario;
+
+// Fabric Method de Pessoa
+public class PessoaFactory
+{
+    public enum TipoPessoa { PROPRIETARIO, ADMINISTRADOR, CLIENTE };
+    
+    public static Pessoa getPessoa(TipoPessoa tipo)
+    {
+        switch(tipo)
+        {
+            case PROPRIETARIO : return (new Proprietario());
+            case ADMINISTRADOR: return (new Administrador());
+            case CLIENTE      : return (new Cliente());
+        }
+        
+        return null;
+    }
+}
