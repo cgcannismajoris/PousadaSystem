17a4826b873d7d6298db0f065bf9f5e86f2452b8
cristianmello <cristianc.mello@gmail.com>
2015-06-07 22:50:43 -0300

    - FRONT-END de DASHBOARD de Admin;

--- src/java/controle/ManageBeanAdmin.java
+++ src/java/controle/ManageBeanAdmin.java
@@ -1,16 +1,67 @@
 package controle;
 
 // ManageBean específico para Administrador
-
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.event.AjaxBehaviorEvent;
 import modelo.Administrador;
 
-@ManagedBean(name="adminMB")
+@ManagedBean(name = "adminMB")
 @RequestScoped
 public class ManageBeanAdmin
 {
+
     private Administrador admin;
+
+    public final byte PAINEL_PERFIL = 1;
+    public final byte PAINEL_GER_EQUIPS = 2;
+    public final byte PAINEL_GER_CHALE = 3;
+
+    private String strExibePerfil = "active";     // "active" ou ""
+    private String strExibeGerEquips = "";
+    private String strExibeGerChale = "";
+
+    private boolean boolExibePerfil = true;
+    private boolean boolExibeGerEquips;
+    private boolean boolExibeGerChale;
+    
+    public void ativarPainelPerfil(AjaxBehaviorEvent event)
+    {
+        this.trocarPainel(PAINEL_PERFIL);
+    }
+    
+    public void ativarPainelGerEquips(AjaxBehaviorEvent event)
+    {
+        this.trocarPainel(PAINEL_GER_EQUIPS);
+    }
+
+    private void trocarPainel(byte painel)
+    {
+        switch (painel)
+        {
+            case PAINEL_PERFIL:
+            {
+                this.strExibePerfil = "active";
+                this.strExibeGerEquips = "";
+                this.strExibeGerChale = "";
+
+                this.boolExibePerfil = true;
+                this.boolExibeGerEquips = false;
+                this.boolExibeGerChale = false;
+            } break;
+
+            case PAINEL_GER_EQUIPS:
+            {
+                this.strExibePerfil = "";
+                this.strExibeGerEquips = "active";
+                this.strExibeGerChale = "";
+
+                this.boolExibePerfil = false;
+                this.boolExibeGerEquips = true;
+                this.boolExibeGerChale = false;
+            } break;
+        }
+    }
 
     public Administrador getAdmin()
     {
@@ -21,5 +72,79 @@ public class ManageBeanAdmin
     {
         this.admin = admin;
     }
-    
+
+    public String getStrExibePerfil()
+    {
+        return strExibePerfil;
+    }
+
+    public void setStrExibePerfil(String strExibePerfil)
+    {
+        this.strExibePerfil = strExibePerfil;
+    }
+
+    public String getStrExibeGerEquips()
+    {
+        return strExibeGerEquips;
+    }
+
+    public void setStrExibeGerEquips(String strExibeGerEquips)
+    {
+        this.strExibeGerEquips = strExibeGerEquips;
+    }
+
+    public String getStrExibeGerChale()
+    {
+        return strExibeGerChale;
+    }
+
+    public void setStrExibeGerChale(String strExibeGerChale)
+    {
+        this.strExibeGerChale = strExibeGerChale;
+    }
+
+    public boolean isBoolExibePerfil()
+    {
+        return boolExibePerfil;
+    }
+
+    public void setBoolExibePerfil(boolean boolExibePerfil)
+    {
+        this.boolExibePerfil = boolExibePerfil;
+    }
+
+    public boolean isBoolExibeGerEquips()
+    {
+        return boolExibeGerEquips;
+    }
+
+    public void setBoolExibeGerEquips(boolean boolExibeGerEquips)
+    {
+        this.boolExibeGerEquips = boolExibeGerEquips;
+    }
+
+    public boolean isBoolExibeGerChale()
+    {
+        return boolExibeGerChale;
+    }
+
+    public void setBoolExibeGerChale(boolean boolExibeGerChale)
+    {
+        this.boolExibeGerChale = boolExibeGerChale;
+    }
+
+    public byte getPAINEL_PERFIL()
+    {
+        return PAINEL_PERFIL;
+    }
+
+    public byte getPAINEL_GER_EQUIPS()
+    {
+        return PAINEL_GER_EQUIPS;
+    }
+
+    public byte getPAINEL_GER_CHALE()
+    {
+        return PAINEL_GER_CHALE;
+    }
 }
